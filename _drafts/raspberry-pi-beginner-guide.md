54f93e759a34c9503894c8be7b381fef0f990588
derekstride <djgstride@gmail.com>
2021-07-14 09:46:20 -0400

    Remove old site data

--- _drafts/raspberry-pi-beginner-guide.md
+++
@@ -1,8 +0,0 @@
----
-title:      A Beginners Guide to the Raspberry Pi
-menu:
-- header: Introduction
-  url:    introduction
----
-
-I'll go over in detail how to set up the raspberry pi and how to start using it.
--- _includes/code_snippets/sysc2003/imports.c
+++
@@ -1,8 +0,0 @@
-/*
- *  This file holds all the symbols you'll need to use
- *  for all the HC12 ports along with other things.
- */
-#include "hcs12dp256.h"
-#include "basicLCD.h"
-#include <stdio.h>
-#include <stdlib.h>
